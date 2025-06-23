93c0e6e18c2a966e38b4c18a2562d8946774646c
Arpit Agrawal <agrawalarpit456@gmail.com>
2025-06-23 09:11:08 +0530

    Project Objective Define and Sample File Setup

--- File Operation CLI Project/index.js
+++ File Operation CLI Project/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs') ;
+// Create a command line interface that lets the user specify a file path and the nodejs process counts the number of words , sentences inside it and also tells whether a given word is present in it or not .
+// Input - node index.js /Users/arpit/file.txt
+// Output - You have 10 words in this file
 
-const { Command } = require('commander') ;
-const program = new Command() ;
