2f79be4c598bf57fc2e8d4af6548458ad028f361
Arpit Agrawal <agrawalarpit456@gmail.com>
2025-06-29 12:38:34 +0530

    Fixed the bugs

--- Todo List CLI Project/index.js
+++ Todo List CLI Project/index.js
@@ -1,18 +1,17 @@
-// Importing Modules
+// Importing Required Modules
 
-const fs = require('fs') ;
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
 
-const { Command } = require('commander') ;
-const program = new Command() ;
-
-// Program Info 
+// Program Information
 
 program
     .name('Todo')
-    .description('It is a CLI Based ToDo List')
-    .version('1.0.0')
+    .description('CLI-based ToDo List Application')
+    .version('1.0.0');
 
-// Parse JSON Array Function
+// Function to Parse JSON and Return Array
 
 function parseTodo (result) {
     try {
@@ -30,18 +29,19 @@ function parseTodo (result) {
     }
 }
 
-// Command to Add a New ToDo
+// Command : Add a New ToDo
 
 program
-    .command('Add')
-    .description('It is used to add a todo to the ToDo List')
+    .command('add')
+    .description('It is used to add a new ToDo item')
     .argument('<todo>','It is the todo to be added')
-    .option('-p , --priority <level>' , 'It is the priority level of the todo (High , Medium , Low)' , 'Medium')
-    .option('-g , --group <group>' , 'It is the group of the todo' , 'General')
+    .option('-p , --priority <level>' , 'It is the priority level of the ToDo (High , Medium , Low)' , 'Medium')
+    .option('-g , --group <group>' , 'It is the group of the ToDo' , 'General')
     .action((todo , options) => {
+
         fs.readFile('./todos.json' , 'utf-8' , (err , result) => {
             if (err) {
-                console.log(`Error Occurred During Reading . \n Error : ${err}`) ;
+                console.log(`Error Occurred During Reading JSON File.\nError : ${err.message}`) ;
             } else {
                 let todoArray = parseTodo(result) ;
 
@@ -54,8 +54,8 @@ program
                 let newTodo = {
                     Id : todoId + 1 ,
                     Todo : todo ,
-                    Priority : option.priority ,
-                    Group : option.group ,
+                    Priority : options.priority ,
+                    Group : options.group ,
                     Status : 'Incomplete'
                 }
 
@@ -63,7 +63,7 @@ program
 
                 fs.writeFile('./todos.json' , JSON.stringify(todoArray,null,2) , (err) => {
                     if (err) {
-                        console.log('Error Occurred ') ;
+                        console.log('Error Occurred During Addition of the New ToDo') ;
                     } else {
                         console.log('ToDo Added Successfully') ;
                     }
@@ -73,19 +73,19 @@ program
         })
     }) ;
 
-// Command to Update a Todo
+// Command : Update an existing Todo
 
 program
-    .command('Update')
-    .description('It is used to update a todo to the ToDo List')
+    .command('update')
+    .description('It is used to update an existing ToDo ')
     .argument('<id>','ID of the todo to be updated')
-    .option('-t , --todo <todo>','It is the todo to be updated')
-    .option('-p , --priority <level>' , 'It is the updated priority level of the todo (High , Medium , Low)' )
-    .option('-g , --group <group>' , 'It is the updated group of the todo')
-    .action((id , option) => {
+    .option('-t , --todo <todo>','Updated ToDo Task')
+    .option('-p , --priority <level>' , 'Updated priority level' )
+    .option('-g , --group <group>' , 'Updated ToDo Group')
+    .action((id , options) => {
         fs.readFile('./todos.json' , 'utf-8' , (err , result) => {
             if (err) {
-                console.log(`Error Occurred During Reading . \n Error : ${err}`) ;
+                console.log(`Error Occurred During Reading JSON File.\nError : ${err.message}`) ;
             } else {
                 let todoArray = parseTodo(result) ;
 
@@ -97,13 +97,13 @@ program
                     if (updateTodoIndex == -1) {
                         console.log('Todo does not exist') ;
                     } else {
-                        todoArray[updateTodoIndex].Todo = option.todo ?? todoArray[updateTodoIndex].Todo ;
-                        todoArray[updateTodoIndex].Priority = option.priority ?? todoArray[updateTodoIndex].Priority ;
-                        todoArray[updateTodoIndex].Group = option.group ?? todoArray[updateTodoIndex].Group ;
+                        todoArray[updateTodoIndex].Todo = options.todo ?? todoArray[updateTodoIndex].Todo ;
+                        todoArray[updateTodoIndex].Priority = options.priority ?? todoArray[updateTodoIndex].Priority ;
+                        todoArray[updateTodoIndex].Group = options.group ?? todoArray[updateTodoIndex].Group ;
 
                         fs.writeFile('./todos.json' , JSON.stringify(todoArray,null,2) , (err) => {
                             if (err) {
-                                console.log('Error Occurred ') ;
+                                console.log('Error Occurred During Updation of the New ToDo') ;
                             } else {
                                 console.log('ToDo Updated Successfully') ;
                             }
@@ -115,16 +115,16 @@ program
     }) ;
 
 
-// Command to mark a Todo as complete
+// Command : Mark A ToDo Status as Complete
 
 program
     .command('mark')
-    .description('It is used to mark a todo as complete')
-    .argument('<id>','ID of the todo to be updated')
+    .description('It is used to mark a ToDo as complete')
+    .argument('<id>','ID of the ToDo to be marked complete')
     .action((id) => {
         fs.readFile('./todos.json' , 'utf-8' , (err , result) => {
             if (err) {
-                console.log(`Error Occurred During Reading . \n Error : ${err}`) ;
+                console.log(`Error Occurred During Reading JSON File.\nError : ${err}`) ;
             } else {
                 let todoArray = parseTodo(result) ;
 
@@ -140,7 +140,7 @@ program
 
                         fs.writeFile('./todos.json' , JSON.stringify(todoArray,null,2) , (err) => {
                             if (err) {
-                                console.log('Error Occurred ') ;
+                                console.log('Error Occurred During Marking a ToDo') ;
                             } else {
                                 console.log('ToDo Marked Successfully') ;
                             }
@@ -160,7 +160,7 @@ program
     .action((id) => {
         fs.readFile('./todos.json' , 'utf-8' , (err , result) => {
             if (err) {
-                console.log(`Error Occurred During Reading . \n Error : ${err}`) ;
+                console.log(`Error Occurred During Reading JSON File.\nError : ${err}`) ;
             } else {
                 let todoArray = parseTodo(result) ;
 
@@ -177,9 +177,9 @@ program
 
                         fs.writeFile('./todos.json' , JSON.stringify(todoArray,null,2) , (err) => {
                             if (err) {
-                                console.log('Error Occurred ') ;
+                                console.log('Error Occurred During Deletion of ToDo') ;
                             } else {
-                                console.log('ToDo Marked Successfully') ;
+                                console.log('ToDo Deleted Successfully') ;
                             }
                         })
                     }
@@ -188,18 +188,18 @@ program
         })
     }) ;
 
-// Command to Display Todo with different options 
+// Command : Display ToDo According To Requirements
 
 program
     .command('display')
     .description('It is used to display a todo ')
-    .option('-i , --id <ID>','ID of the Todo to be Displayed Alone')
-    .option('-p , --priority <level>',' Priority Level of Todo to be Displayed ')
-    .option('-g , --group <group> ' , 'Group of the Todo to be Displayed')
-    .action((option) => {
+    .option('-i , --id <ID>','ID of the Todo to be Displayed ')
+    .option('-p , --priority <level>',' Priority Level of Todos to be Displayed ')
+    .option('-g , --group <group> ' , 'Group of the Todos to be Displayed')
+    .action((options) => {
         fs.readFile('./todos.json' , 'utf-8' , (err , result) => {
             if (err) {
-                console.log(`Error Occurred During Reading . \n Error : ${err}`) ;
+                console.log(`Error Occurred During Reading JSON File.\n Error : ${err}`) ;
             } else {
                 let todoArray = parseTodo(result) ;
 
@@ -207,24 +207,17 @@ program
                     console.log('Todo List is empty . Please add a new todo') ;
                 } else {
 
-                    let todoArray = parseTodo(result)
-
-                    if (todoArray.length === 0) {
-                        console.log('ToDo List is Empty . Please Add a Todo to the ToDo List');
-                        return 
-                    }
-
-                    if (option.priority) {
-                        displayArray = todoArray.filter((todoObj) => (todoObj.Priority == option.priority))
+                    if (options.priority) {
+                        displayArray = todoArray.filter((todoObj) => (todoObj.Priority == options.priority)) ;
                         if (displayArray.length == 0) {
-                            console.log('No ToDo Belonging To The Specified Priority')
+                            console.log('No ToDo Belonging To The Specified Priority') ;
                         } else {
-                            console.log(displayArray)
+                            console.log(displayArray) ;
                         }
                     }
 
-                    if (option.group) {
-                        displayArray = todoArray.filter((todoObj) => (todoObj.Group == option.group))
+                    if (options.group) {
+                        displayArray = todoArray.filter((todoObj) => (todoObj.Group == options.group))
                         if (displayArray.length == 0) {
                             console.log('No ToDo Belonging To The Specified Group')
                         } else {
@@ -232,8 +225,8 @@ program
                         }        
                     }
 
-                    if (option.id) {
-                        displayArray = todoArray.filter((todoObj) => (todoObj.Id == option.id))
+                    if (options.id) {
+                        displayArray = todoArray.filter((todoObj) => (todoObj.Id == options.id))
                         if (displayArray.length == 0) {
                             console.log('No ToDo Belonging To The Specified ID')
                         } else {
@@ -241,7 +234,7 @@ program
                         }  
                     }
 
-                    if (Object.keys(option).length == 0 ) {
+                    if (Object.keys(options).length == 0 ) {
                         todoArray.forEach((todoObj) => {
                             console.log(todoObj)
                         })
